5a4044eb4c45e11fccb7bb015a7e34984c10feb8
edge <chen458438184@qq.com>
2021-05-27 13:24:11 +0800

    mysql-test

--- test12_nx.py
+++ test12_nx.py
@@ -82,118 +82,118 @@ def open_cam_onboard(width, height):
 	return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
 
 def open_cam_rtsp(uri, width, height, latency):
-    gst_str = ('rtspsrc location={} latency={} ! '
-               'rtph264depay ! h264parse ! omxh264dec ! '
-               'nvvidconv ! '
-               'video/x-raw, width=(int){}, height=(int){}, '
-               'format=(string)BGRx ! '
-               'videoconvert ! appsink').format(uri, latency, width, height)
-    return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+	gst_str = ('rtspsrc location={} latency={} ! '
+			   'rtph264depay ! h264parse ! omxh264dec ! '
+			   'nvvidconv ! '
+			   'video/x-raw, width=(int){}, height=(int){}, '
+			   'format=(string)BGRx ! '
+			   'videoconvert ! appsink').format(uri, latency, width, height)
+	return cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
 
 
 def box_iou2(a, b):
-    '''
-    Helper funciton to calculate the ratio between intersection and the union of
-    two boxes a and b
-    a[0], a[1], a[2], a[3] <-> left, up, right, bottom
-    '''
-    
-    w_intsec = np.maximum (0, (np.minimum(a[1][0], b[1][0]) - np.maximum(a[0][0], b[0][0])))
-    h_intsec = np.maximum (0, (np.minimum(a[1][1], b[1][1]) - np.maximum(a[0][1], b[0][1])))
-    s_intsec = w_intsec * h_intsec
-    s_a = (a[1][0] - a[0][0])*(a[1][1] - a[0][1])
-    s_b = (b[1][0] - b[0][0])*(b[1][1] - b[0][1])
+	'''
+	Helper funciton to calculate the ratio between intersection and the union of
+	two boxes a and b
+	a[0], a[1], a[2], a[3] <-> left, up, right, bottom
+	'''
+	
+	w_intsec = np.maximum (0, (np.minimum(a[1][0], b[1][0]) - np.maximum(a[0][0], b[0][0])))
+	h_intsec = np.maximum (0, (np.minimum(a[1][1], b[1][1]) - np.maximum(a[0][1], b[0][1])))
+	s_intsec = w_intsec * h_intsec
+	s_a = (a[1][0] - a[0][0])*(a[1][1] - a[0][1])
+	s_b = (b[1][0] - b[0][0])*(b[1][1] - b[0][1])
   
-    return float(s_intsec)/(s_a + s_b -s_intsec)
+	return float(s_intsec)/(s_a + s_b -s_intsec)
 
 def assign_detections_to_trackers(trackers, detections, iou_thrd = 0.3):
-    '''
-    From current list of trackers and new detections, output matched detections,
-    unmatchted trackers, unmatched detections.
-    '''    
-    
-    IOU_mat= np.zeros((len(trackers),len(detections)),dtype=np.float32)
-    for t,trk in enumerate(trackers):
-        #trk = convert_to_cv2bbox(trk) 
-        for d,det in enumerate(detections):
-         #   det = convert_to_cv2bbox(det)
-            IOU_mat[t,d] = box_iou2(trk,det) 
-    
-    # Produces matches       
-    # Solve the maximizing the sum of IOU assignment problem using the
-    # Hungarian algorithm (also known as Munkres algorithm)
-    
-    matched_idx_tra, matched_idx_det = linear_assignment(-IOU_mat)        
-    matched_idx = np.zeros((len(matched_idx_tra),2),dtype=np.int8)
-    for i in range(len(matched_idx_tra)):
-        matched_idx[i]=(matched_idx_tra[i],matched_idx_det[i])
-       
+	'''
+	From current list of trackers and new detections, output matched detections,
+	unmatchted trackers, unmatched detections.
+	'''    
+	
+	IOU_mat= np.zeros((len(trackers),len(detections)),dtype=np.float32)
+	for t,trk in enumerate(trackers):
+		#trk = convert_to_cv2bbox(trk) 
+		for d,det in enumerate(detections):
+		 #   det = convert_to_cv2bbox(det)
+			IOU_mat[t,d] = box_iou2(trk,det) 
+	
+	# Produces matches       
+	# Solve the maximizing the sum of IOU assignment problem using the
+	# Hungarian algorithm (also known as Munkres algorithm)
+	
+	matched_idx_tra, matched_idx_det = linear_assignment(-IOU_mat)        
+	matched_idx = np.zeros((len(matched_idx_tra),2),dtype=np.int8)
+	for i in range(len(matched_idx_tra)):
+		matched_idx[i]=(matched_idx_tra[i],matched_idx_det[i])
+	   
 
-    unmatched_trackers, unmatched_detections = [], []
-    for t,trk in enumerate(trackers):
-        if(t not in matched_idx[:,0]):
-            unmatched_trackers.append(t)
+	unmatched_trackers, unmatched_detections = [], []
+	for t,trk in enumerate(trackers):
+		if(t not in matched_idx[:,0]):
+			unmatched_trackers.append(t)
 
-    for d, det in enumerate(detections):
-        if(d not in matched_idx[:,1]):
-            unmatched_detections.append(d)
+	for d, det in enumerate(detections):
+		if(d not in matched_idx[:,1]):
+			unmatched_detections.append(d)
 
-    matches = []
+	matches = []
    
-    # For creating trackers we consider any detection with an 
-    # overlap less than iou_thrd to signifiy the existence of 
-    # an untracked object
-    
-    for m in matched_idx:
-        if(IOU_mat[m[0],m[1]]<iou_thrd):
-            unmatched_trackers.append(m[0])
-            unmatched_detections.append(m[1])
-        else:
-            matches.append(m.reshape(1,2))
-    
-    if(len(matches)==0):
-        matches = np.empty((0,2),dtype=int)
-    else:
-        matches = np.concatenate(matches,axis=0)
-    
-    return matches, np.array(unmatched_detections), np.array(unmatched_trackers)
+	# For creating trackers we consider any detection with an 
+	# overlap less than iou_thrd to signifiy the existence of 
+	# an untracked object
+	
+	for m in matched_idx:
+		if(IOU_mat[m[0],m[1]]<iou_thrd):
+			unmatched_trackers.append(m[0])
+			unmatched_detections.append(m[1])
+		else:
+			matches.append(m.reshape(1,2))
+	
+	if(len(matches)==0):
+		matches = np.empty((0,2),dtype=int)
+	else:
+		matches = np.concatenate(matches,axis=0)
+	
+	return matches, np.array(unmatched_detections), np.array(unmatched_trackers)
 
 def draw_box_label(img, bbox_cv2, box_color=(0, 255, 255), personReID_info={'personID':'Unknown'}, show_label=True):
-    '''
-    Helper funciton for drawing the bounding boxes and the labels
-    bbox_cv2 = [left, top, right, bottom]
-    '''
-    #box_color= (0, 255, 255)
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    font_size = 0.8
-    font_color = (0, 0, 255)
-    left, top, right, bottom = bbox_cv2[0], bbox_cv2[1], bbox_cv2[2], bbox_cv2[3]
-    
-    # Draw the bounding box
-    cv2.rectangle(img, (left, top), (right, bottom), box_color, 4)
-    
-    if show_label:
-        # Draw a filled box on top of the bounding box (as the background for the labels)
-        cv2.rectangle(img, (left-2, top-10), (right+2, top), box_color, -1, 1)
-        
-        # Output the labels that show the x and y coordinates of the bounding box center.
-        text_ID = 'PersonID:' + personReID_info['personID']
-        cv2.putText(img,text_ID,(left,top), font, font_size, font_color, 2, cv2.LINE_AA)
-        #text_x= 'x='+str((left+right)/2)
-        #cv2.putText(img,text_x,(left,top-10), font, font_size, font_color, 1, cv2.LINE_AA)
-        #text_y= 'y='+str((top+bottom)/2)
-        #cv2.putText(img,text_y,(left,top), font, font_size, font_color, 1, cv2.LINE_AA)
-            
-    return img    
+	'''
+	Helper funciton for drawing the bounding boxes and the labels
+	bbox_cv2 = [left, top, right, bottom]
+	'''
+	#box_color= (0, 255, 255)
+	font = cv2.FONT_HERSHEY_SIMPLEX
+	font_size = 0.8
+	font_color = (0, 0, 255)
+	left, top, right, bottom = bbox_cv2[0], bbox_cv2[1], bbox_cv2[2], bbox_cv2[3]
+	
+	# Draw the bounding box
+	cv2.rectangle(img, (left, top), (right, bottom), box_color, 4)
+	
+	if show_label:
+		# Draw a filled box on top of the bounding box (as the background for the labels)
+		cv2.rectangle(img, (left-2, top-10), (right+2, top), box_color, -1, 1)
+		
+		# Output the labels that show the x and y coordinates of the bounding box center.
+		text_ID = 'PersonID:' + personReID_info['personID']
+		cv2.putText(img,text_ID,(left,top), font, font_size, font_color, 2, cv2.LINE_AA)
+		#text_x= 'x='+str((left+right)/2)
+		#cv2.putText(img,text_x,(left,top-10), font, font_size, font_color, 1, cv2.LINE_AA)
+		#text_y= 'y='+str((top+bottom)/2)
+		#cv2.putText(img,text_y,(left,top), font, font_size, font_color, 1, cv2.LINE_AA)
+			
+	return img    
 
 def db_connection():
-    mydb = mysql.connector.connect( host = '192.168.1.100',
-    user = 'cqy',
-    port = '3306',
-    database = 'edgedemo',
-    passwd = '123456',
-    autocommit = True)
-    return mydb
+	mydb = mysql.connector.connect( host = '192.168.1.100',
+	user = 'cqy',
+	port = '3306',
+	database = 'edgedemo',
+	passwd = '123456',
+	autocommit = True)
+	return mydb
 
 def handle_frames(frame):
 
@@ -210,7 +210,7 @@ def handle_frames(frame):
 	if len(tracker_list) > 0:
 		for trk in tracker_list:
 			x_box.append([(trk.box[0],trk.box[1]),(trk.box[2],trk.box[3])]) #should be changed into the right format instead of the .box format
-            
+			
 	matched, unmatched_dets, unmatched_trks = assign_detections_to_trackers(x_box, detection_results, iou_thrd = 0.2)  
 	
 	# Deal with matched detections     
@@ -227,7 +227,7 @@ def handle_frames(frame):
 			tmp_trk.hits += 1
 			tmp_trk.no_losses = 0
 	
-    # Deal with unmatched detections      
+	# Deal with unmatched detections      
 	if len(unmatched_dets)>0:
 		for idx in unmatched_dets:
 			z = detection_results[idx]
@@ -237,13 +237,13 @@ def handle_frames(frame):
 			y2 = int(z[1][1])
 			person = frame[y1:y2, x1:x2, :]
 			identify_name, score = compare.run(person, origin_f, origin_name)
-            c1 = 118
-            #connect to database
-            mydb = db_connection()
-            cur = mydb.cursor()
-            info = "insert into REID(person,ctime, camera) values ('{0}',{1},{2})".format(identify_name, int(time.time()), c1)
-            cur.execute(info)
-            mydb.close()
+			c1 = 118
+			#connect to database
+			mydb = db_connection()
+			cur = mydb.cursor()
+			info = "insert into REID(person,ctime, camera) values ('{0}',{1},{2})".format(identify_name, int(time.time()), c1)
+			cur.execute(info)
+			mydb.close()
 			if(identify_name in [ "MJ1","MJ2","MJ3","MJ4","MJ5","MJ6","MJ7","MJ8","MJ9","MJ10","MJ11","MJ12","MJ13","MJ14"]):
 				identify_name = "Person_1"
 				c1 = 118
@@ -257,7 +257,7 @@ def handle_frames(frame):
 			#	identify_name = "Person_2"
 			print("identify name:{}, score:{}".format(identify_name, round(1-score, 2)))
 			
-            #generate a new tracker for the person
+			#generate a new tracker for the person
 			z = np.expand_dims([n for a in z for n in a], axis=0).T
 			tmp_trk = Tracker() # Create a new tracker
 			x = np.array([[z[0], 0, z[1], 0, z[2], 0, z[3], 0]]).T
@@ -272,7 +272,7 @@ def handle_frames(frame):
 			tracker_list.append(tmp_trk)
 			x_box.append(xx)
 	
-	    # Deal with unmatched tracks       
+		# Deal with unmatched tracks       
 	if len(unmatched_trks)>0:
 		for trk_idx in unmatched_trks:
 			tmp_trk = tracker_list[trk_idx]
@@ -326,9 +326,9 @@ def handle_frames(frame):
 # 		
 # 		bounding_boxs.append([(x1,y1), (x2,y2), identify_name+' '+str(round(1-score, 2))])
 # 		#img = cam_detection.draw_rectangle(img, (x1,y1,x2,y2), identify_name+'  '+str(round((1-score), 2)))
-        
-        
-    
+		
+		
+	
 			
 # 	for obj in bounding_boxs:
 # 		print(obj)
@@ -410,7 +410,7 @@ def gen_frames():  # generate frame by frame from camera
 		# if the `q` key was pressed, break from the loop
 		#if key == ord("q"):
 		#	break
-    
+	
 @app.route('/video_feed')
 def video_feed():
 	#Video streaming route. Put this in the src attribute of an img tag
